c853057e6189ba76d252fad3415690f44781a389
kat-gie <gielenkatie@gmail.com>
2016-11-15 20:38:29 -0600

    14 validating user input

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -8,7 +8,10 @@ class QuotesController < ApplicationController
   end 
 
   def create 
-    Quote.create(quote_params)
+    @quote = Quote.create(quote_params)
+    if @quote.invalid?
+      flash[:error] = '<strong>Could not save</strong>, you must enter between 3 and 300 characters.'
+    end
     redirect_to root_path
   end
 
